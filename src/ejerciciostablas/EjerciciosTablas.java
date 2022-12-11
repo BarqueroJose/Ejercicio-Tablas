60f254b5261058d3074626624243a76a99f6c31c
BarqueroJose <josebarquerolazo12@gmail.com>
2022-12-11 12:00:59 -0600

    Cargando Trabajo

---
+++ src/ejerciciostablas/EjerciciosTablas.java
@@ -0,0 +1,18 @@
+
+package ejerciciostablas;
+
+/**
+ *
+ * @author barquero
+ */
+public class EjerciciosTablas {
+
+    
+    public static void main(String[] args) {
+        frmPrincipal oP = new frmPrincipal();
+        oP.setVisible(true);
+        oP.setLocationRelativeTo(null);
+              
+    }
+    
+}
